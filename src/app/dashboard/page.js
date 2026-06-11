4537fec8096dc6e5d27e28c219a1e282466d21c9
sanath2003git <165697113+sanath2003git@users.noreply.github.com>
2026-06-12 01:24:23 +0530

    Initial Next.js project setup and home page

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,15 @@
+// src/app/dashboard/page.js
+
+export default function Dashboard() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold">
+        Dashboard
+      </h1>
+
+      <p>
+        Welcome to Store Management Dashboard
+      </p>
+    </div>
+  );
+}
