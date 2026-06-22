c947eb49200196f5980cc9744d0132c39e651b67
C S Sanath Sreekumar <165697113+sanath2003git@users.noreply.github.com>
2026-06-22 23:25:12 +0530

    Connected MongoDB Atlas with Next.js using Mongoose

---
+++ src/app/api/test/route.js
@@ -0,0 +1,31 @@
+import { connectDB } from "@/lib/mongodb";
+
+export async function GET() {
+
+  try {
+
+    await connectDB();
+
+    return Response.json({
+
+      success:true,
+
+      message:"MongoDB Connected Successfully"
+
+    });
+
+  }
+
+  catch(error){
+
+    return Response.json({
+
+      success:false,
+
+      message:error.message
+
+    });
+
+  }
+
+}
